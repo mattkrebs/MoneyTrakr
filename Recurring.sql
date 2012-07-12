e2aa8b2929ad3cb2643984931d92eb270b5ba824
mattkrebs <matt@44lights.net>
2012-07-12 17:05:03 -0500

    Table Scripts

---
+++ Recurring.sql
@@ -0,0 +1,27 @@
+USE [MoneyTrakr]
+GO
+
+/****** Object:  Table [dbo].[Recurring]    Script Date: 07/12/2012 17:04:13 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Recurring](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](200) NOT NULL,
+	[StartDate] [datetime] NOT NULL,
+	[Automatic] [bit] NOT NULL,
+	[RecurWeekly] [bit] NOT NULL,
+	[RecurBiWeekly] [bit] NOT NULL,
+	[RecurMontly] [bit] NOT NULL,
+	[Amount] [decimal](18, 0) NOT NULL,
+ CONSTRAINT [PK_Recurring] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
---
+++ User.sql
@@ -0,0 +1,25 @@
+USE [MoneyTrakr]
+GO
+
+/****** Object:  Table [dbo].[User]    Script Date: 07/12/2012 17:04:43 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[User](
+	[ID] [int] NOT NULL,
+	[UserName] [nvarchar](50) NOT NULL,
+	[EmailAddress] [nvarchar](255) NOT NULL,
+	[Password] [nvarchar](50) NOT NULL,
+	[CreatedDate] [datetime] NOT NULL,
+	[AccountID] [int] NOT NULL,
+ CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
